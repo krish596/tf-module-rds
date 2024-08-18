18a56ca3b455ba3ecd9006dced16807ac63193b0
Chimtamreddy <kr7348202@gmail.com>
2024-08-18 11:48:48 +0530

    Terraform

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "tags" {}
+variable "env" {}
+variable "rds_type" {}
+variable "db_port" {}
+variable "engine" {}
+variable "engine_family" {}
+variable "engine_version" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+
+
+
