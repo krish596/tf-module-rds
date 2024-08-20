cbdcf4572b55a25ff767086cbe1b1ede8484ad91
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 09:38:10 +0530

    Terraform

--- vars.tf
+++ vars.tf
@@ -1,17 +1,17 @@
-# variable "tags" {}
-# variable "env" {}
-# variable "rds_type" {}
-# variable "db_port" {}
-# variable "engine" {}
-# variable "engine_family" {}
-# variable "engine_version" {}
-# variable "backup_retention_period" {}
-# variable "preferred_backup_window" {}
-# variable "skip_final_snapshot" {}
-# variable "subnet_ids" {}
-# variable "vpc_id" {}
-# variable "sg_ingress_cidr" {}
-# variable "instance_count" {}
-# variable "instance_class" {}
-#
-#
+variable "tags" {}
+variable "env" {}
+variable "rds_type" {}
+variable "db_port" {}
+variable "engine" {}
+variable "engine_family" {}
+variable "engine_version" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "instance_count" {}
+variable "instance_class" {}
+
+
